8d29b72b24d5edc6d4d30336dd221d761fd13d2f
Ana Duran <ana.duran-b@mail.escuelaing.edu.co>
2024-09-23 18:13:00 -0500

    inicialización

---
+++ src/test/java/com/cargomaze/cargo_maze/CargoMazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cargomaze.cargo_maze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoMazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
